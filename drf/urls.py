2da8517da60284f44d873468630d79f9721e00a4
RamonCarrillo90 <carrilloriverar70@gmail.com>
2025-11-30 14:48:35 -0700

    actualizamos las rutas de las urls

--- drf/urls.py
+++ drf/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('recognition.urls')),
+    path('api/', include('api.urls')),  # ✅ Esta sí existe
+    # path('api/', include('recognition.urls')),  # ❌ COMENTAR O ELIMINAR ESTA LÍNEA
 ]
